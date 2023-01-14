3d53fc5736ef51e4e419b820b67e96da2809f789
NehaTShigvan <121689166+NehaTShigvan@users.noreply.github.com>
2023-01-14 23:16:52 +0530

    Add files via upload

---
+++ CurrencyConverter.py
@@ -0,0 +1,16 @@
+
+with open('text.txt') as f:
+    data = f.readlines()
+
+dict = {}
+for line in data:
+    lines = line.split('\t')
+    dict[lines[0]] = lines[1]
+
+amount = int(input("Enter the amount to be converted: "))  
+print("The available options to convert currency are as follows:\n")
+[print(item) for item in dict.keys()]  
+currency = input("Enter the name of a currency: ")    
+
+print(f"{amount} INR is equal to {amount*float(dict[currency])} {currency}")
+#type cast -->since dict value is in string data type it should be converted into float 
